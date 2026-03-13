f50afea2aa5616e8aba2f2a2498231058da09841
Den1 <zeromen945@gmail.com>
2026-03-13 08:48:26 +0000

    Add repositories to root build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
+// Корневой build.gradle.kts
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.android.library") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+// ЭТОГО БЛОКА ТЕБЕ НЕ ХВАТАЛО:
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
