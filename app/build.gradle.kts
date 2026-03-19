947366a56f1dc9cd40c9d21dad6bb2f8cc95ddb7
Den1108 <zeromen945@gmail.com>
2026-03-19 13:14:17 +0200

    1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 24
         targetSdk = 34
         // Увеличивай эти значения при каждом обновлении APK
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         externalNativeBuild {
             cmake {
