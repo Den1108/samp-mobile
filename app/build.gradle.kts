bfbcf00cae2a2955ab07138c476d39d2c00a79fb
Den1108 <zeromen945@gmail.com>
2026-03-24 09:57:25 +0200

    Добавлени либи

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 24
         targetSdk = 34
         // Увеличивай эти значения при каждом обновлении APK
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         externalNativeBuild {
             cmake {
