52113e871a00aab6654755e54229446fbe51b8ed
Den1108 <zeromen945@gmail.com>
2026-05-12 09:33:05 +0300

    исправление 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,14 @@ pluginManagement {
     }
 }
 
+// Репозитории для зависимостей проекта (сюда переехало из allprojects в build.gradle.kts)
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "samp-mobile"
 include(":app")
